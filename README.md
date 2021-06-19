a07ef0aea6670ce73f92ae49c7b19084409a95a2
root <root@fv-az163-821.kzyshwy3mise5klwsllqwzgzyb.cx.internal.cloudapp.net>
2021-06-19 07:43:45 +0000

    Add extras for full_oppo6771_18611-user 9 PPR1.180610.011 eng.root.20200722.215443 release-keys
    Signed-off-by: root <root@fv-az163-821.kzyshwy3mise5klwsllqwzgzyb.cx.internal.cloudapp.net>

---
+++ README.md
@@ -0,0 +1,21 @@
+## full_oppo6771_18611-user 9 PPR1.180610.011 eng.root.20200722.215443 release-keys
+- Manufacturer: realme
+- Platform: mt6771
+- Codename: RMX1831
+- Brand: Realme
+- Flavor: full_oppo6771_18611-user
+- Release Version: 9
+- Id: PPR1.180610.011
+- Incremental: 1595426322
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: en-US
+- Screen Density: undefined
+- Fingerprint: 
+- OTA version: 
+- Branch: full_oppo6771_18611-user-9-PPR1.180610.011-eng.root.20200722.215443-release-keys
+- Repo: realme_rmx1831_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cat system/system/priv-app/OppoGallery2/OppoGallery2.apk.* 2>/dev/null >> system/system/priv-app/OppoGallery2/OppoGallery2.apk
+rm -f system/system/priv-app/OppoGallery2/OppoGallery2.apk.* 2>/dev/null
+cat system/system/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> system/system/priv-app/Velvet/Velvet.apk
+rm -f system/system/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat system/system/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> system/system/priv-app/GmsCore/GmsCore.apk
+rm -f system/system/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat system/system/priv-app/SystemUI/SystemUI.apk.* 2>/dev/null >> system/system/priv-app/SystemUI/SystemUI.apk
+rm -f system/system/priv-app/SystemUI/SystemUI.apk.* 2>/dev/null
+cat system/system/app/YouTube/YouTube.apk.* 2>/dev/null >> system/system/app/YouTube/YouTube.apk
+rm -f system/system/app/YouTube/YouTube.apk.* 2>/dev/null
+cat system/system/app/GoogleLatinInput/GoogleLatinInput.apk.* 2>/dev/null >> system/system/app/GoogleLatinInput/GoogleLatinInput.apk
+rm -f system/system/app/GoogleLatinInput/GoogleLatinInput.apk.* 2>/dev/null
+cat system/system/app/OppoCamera/OppoCamera.apk.* 2>/dev/null >> system/system/app/OppoCamera/OppoCamera.apk
+rm -f system/system/app/OppoCamera/OppoCamera.apk.* 2>/dev/null
+cat system/system/app/Chrome/Chrome.apk.* 2>/dev/null >> system/system/app/Chrome/Chrome.apk
+rm -f system/system/app/Chrome/Chrome.apk.* 2>/dev/null
